2e8b238eb3f9537ee555d99510e96b6758f28852
Zhi Yuan <1311257372@qq.com>
2025-12-18 14:36:34 +0800

    统一快捷键

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// 侧边栏状态跟踪
+let sidePanelOpen = false;
+
 // 创建右键菜单
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -15,28 +18,41 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 
     // 2. 关键：在当前窗口打开侧边栏
     chrome.sidePanel.open({ windowId: tab.windowId });
+    sidePanelOpen = true;
   }
 });
 
 // 监听点击插件图标 -> 也打开侧边栏
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ windowId: tab.windowId });
+  sidePanelOpen = true;
 });
 
-// 监听快捷键命令
+// 监听侧边栏关闭事件
+chrome.sidePanel.onClosed.addListener(() => {
+  sidePanelOpen = false;
+});
+
+// 监听侧边栏打开事件
+chrome.sidePanel.onOpened.addListener(() => {
+  sidePanelOpen = true;
+});
+
+// 监听快捷键命令 - 切换侧边栏显示状态
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "open-sidepanel") {
+  if (command === "toggle-sidepanel") {
     // 获取当前活动标签页的窗口ID
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs[0]) {
-        chrome.sidePanel.open({ windowId: tabs[0].windowId });
-      }
-    });
-  } else if (command === "close-sidepanel") {
-    // 隐藏侧边栏 - 由于Chrome API限制，我们可以通过聚焦到当前页面来模拟隐藏效果
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) {
-        chrome.sidePanel.close({ windowId: tabs[0].windowId });
+        if (sidePanelOpen) {
+          // 如果侧边栏是打开的，则关闭它
+          chrome.sidePanel.close({ windowId: tabs[0].windowId });
+          sidePanelOpen = false;
+        } else {
+          // 如果侧边栏是关闭的，则打开它
+          chrome.sidePanel.open({ windowId: tabs[0].windowId });
+          sidePanelOpen = true;
+        }
       }
     });
   }
